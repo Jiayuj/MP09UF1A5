6c2cf29bdd03bf7617bea4d2d6f306a401a1ce2c
jiayu <jiayuji@elpuig.xeill.net>
2019-11-21 16:46:22 +0100

    Initial commit

---
+++ src/main/java/Claus.java
@@ -0,0 +1,137 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
+
+public class Claus {
+
+    public static KeyPair randomGenerate(int len) {
+        KeyPair keys = null;
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(len);
+            keys = keyGen.genKeyPair();
+        } catch (Exception ex) {
+            System.err.println("Generador no disponible.");
+        }
+        return keys;
+    }
+
+    public static byte[] encryptData(byte[] data, PublicKey pub) {
+        byte[] encryptedData = null;
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","SunJCE");
+            cipher.init(Cipher.ENCRYPT_MODE, pub);
+            encryptedData =  cipher.doFinal(data);
+        } catch (Exception  ex) {
+            System.err.println("Error xifrant: " + ex);
+        }
+        return encryptedData;
+    }
+
+    public static byte[] decryptData(byte[] data, PrivateKey pri) {
+        byte[] encryptedData = null;
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","SunJCE");
+            cipher.init(Cipher.DECRYPT_MODE, pri);
+            encryptedData =  cipher.doFinal(data);
+        } catch (Exception  ex) {
+            System.err.println("Error xifrant: " + ex);
+        }
+        return encryptedData;
+    }
+
+    public static KeyStore loadKeyStore(String ksFile, String ksPwd) throws Exception {
+        KeyStore ks = KeyStore.getInstance("JCEKS");
+        File f = new File (ksFile);
+        if (f.isFile()) {
+            FileInputStream in = new FileInputStream (f);
+            ks.load(in, ksPwd.toCharArray());
+        }
+        return ks;
+    }
+
+
+
+    public static SecretKey keygenKeyGeneration(int keySize) {
+        SecretKey sKey = null;
+        if ((keySize == 128)||(keySize == 192)||(keySize == 256)) {
+            try {
+                KeyGenerator kgen = KeyGenerator.getInstance("AES");
+                kgen.init(keySize);
+                sKey = kgen.generateKey();
+
+            } catch (NoSuchAlgorithmException ex) {
+                System.err.println("Generador no disponible.");
+            }
+        }
+        return sKey;
+    }
+
+    public static byte[] signData(byte[] data, PrivateKey priv) {
+        byte[] signature = null;
+
+        try {
+            Signature signer = Signature.getInstance("SHA1withRSA");
+            signer.initSign(priv);
+            signer.update(data);
+            signature = signer.sign();
+        } catch (Exception ex) {
+            System.err.println("Error signant les dades: " + ex);
+        }
+        return signature;
+    }
+
+
+    public static boolean validateSignature(byte[] data, byte[] signature, PublicKey pub) {
+        boolean isValid = false;
+        try {
+            Signature signer = Signature.getInstance("SHA1withRSA");
+            signer.initVerify(pub);
+            signer.update(data);
+            isValid = signer.verify(signature);
+        } catch (Exception ex) {
+            System.err.println("Error validant les dades: " + ex);
+        }
+        return isValid;
+    }
+
+    public static byte[][] encryptWrappedData(byte[] data, PublicKey pub) {
+        byte[][] encWrappedData = new byte[2][];
+        try {
+            KeyGenerator kgen = KeyGenerator.getInstance("AES");
+            kgen.init(128);
+            SecretKey sKey = kgen.generateKey();
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, sKey);
+            byte[] encMsg = cipher.doFinal(data);
+            cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.WRAP_MODE, pub);
+            byte[] encKey = cipher.wrap(sKey);
+            encWrappedData[0] = encMsg;
+            encWrappedData[1] = encKey;
+        } catch (Exception  ex) {
+            System.err.println("Ha succeït un error xifrant: " + ex);
+        }
+        return encWrappedData;
+    }
+
+    public static byte[] decryptWrappedData(byte[][] data, PrivateKey pub) {
+        byte[] encMsg = null;
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            cipher.init(Cipher.UNWRAP_MODE, pub); // Inicia el Unwrap de la clave privada
+            Key skey = cipher.unwrap(data[1],"AES",Cipher.SECRET_KEY); // Obtiene la clave privada
+            cipher = Cipher.getInstance("AES"); // Define el Algoritmo de los datos
+            cipher.init(Cipher.DECRYPT_MODE, skey); // Inicia el decrypt de los datos usando la private key que hemos obtenido
+            encMsg = cipher.doFinal(data[0]); // Desencripta los datos
+        } catch (Exception  ex) {
+            System.err.println("Ha succeït un error desxifrant: " + ex);
+        }
+        return encMsg;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,125 @@
+
+import javax.crypto.SecretKey;
+import java.io.*;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.*;
+
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        byte[] dataxifrat;
+        byte[] datadesxifrat;
+        String data;
+
+        System.out.println("Exercicis 1.1");
+        KeyPair keyPair;
+        keyPair = Claus.randomGenerate(1024);
+        System.out.println("datos:  ");
+        data = new Scanner(System.in).nextLine();
+        dataxifrat = Claus.encryptData(data.getBytes(),keyPair.getPublic());
+        System.out.println("data xifrat: "+new String(dataxifrat));
+        datadesxifrat = Claus.decryptData(dataxifrat,keyPair.getPrivate());
+        System.out.println("data desxifrat: "+new String(datadesxifrat));
+        System.out.format("claus Publico: %s "+"\n"+" claus Privat: %s",keyPair.getPublic(),keyPair.getPrivate());
+
+
+        System.out.println("\n\n\nExercicis 1.2");
+
+        KeyStore keyStore = Claus.loadKeyStore("/home/dam2a/.keystore.jks","112123");
+        System.out.println("Tipo: "+keyStore.getType());
+        System.out.println("tamaño: "+keyStore.size());
+        Enumeration<String> enumeration = keyStore.aliases();
+        while (enumeration.hasMoreElements()){
+            System.out.println("Alias del keystore: " + enumeration.nextElement());
+        }
+        System.out.println("Certificado: " + keyStore.getCertificate("jordi"));
+
+        System.out.println("Exercicis 1.2.2");
+        SecretKey secretKey = Claus.keygenKeyGeneration(128);
+
+        // get user password and file input stream
+        String pas = "112123";
+        char[] password = pas.toCharArray();
+
+        try (FileInputStream fis = new FileInputStream("/home/dam2a/.keystore.jks")) {
+            keyStore.load(fis, password);
+        }
+
+        KeyStore.ProtectionParameter protParam = new KeyStore.PasswordProtection(password);
+
+        // save my secret key
+        javax.crypto.SecretKey mySecretKey;
+        KeyStore.SecretKeyEntry skEntry = new KeyStore.SecretKeyEntry(secretKey);
+        keyStore.setEntry("secretKeyAlias", skEntry, protParam);
+
+        // store away the keystore
+        try (FileOutputStream fos = new FileOutputStream("/home/dam2a/.keystore.jks")) {
+            keyStore.store(fos, password);
+        }
+
+
+//        System.out.println("Exercicis 1.3");
+//        FileInputStream fis = new FileInputStream("/home/dam2a/jordi.cer");
+//        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+//        Collection c = cf.generateCertificates(fis);
+//        Iterator i = c.iterator();
+//        while (i.hasNext()) {
+//            Certificate cert = (Certificate)i.next();
+//            System.out.println(cert);
+//        }
+
+
+        System.out.println("\n\n\nExercicis 1.4");
+
+        FileInputStream is = new FileInputStream("/home/dam2a/.keystore");
+        KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+        keystore.load(is, "112123".toCharArray());
+
+        String alias = "mykey";
+
+        Key key = keystore.getKey(alias, "112123".toCharArray());
+        if (key instanceof PrivateKey) {
+            // Get certificate of public key
+            Certificate cert = keystore.getCertificate(alias);
+
+            // Get public key
+            PublicKey publicKey = cert.getPublicKey();
+            System.out.println(publicKey.toString());
+        }
+
+        System.out.println("\n\n\nExercicis 1.5");
+
+        byte[] dataBy = "qweqwe".getBytes();
+        PrivateKey privKey = keyPair.getPrivate();
+        byte[] firma = Claus.signData(dataBy,privKey);
+        System.out.println(new String(firma));
+
+        System.out.println("\n\n\nExercicis 1.6");
+        PublicKey publicKey = keyPair.getPublic();
+        boolean verificado = Claus.validateSignature(dataBy,firma,publicKey);
+        System.out.println(verificado);
+
+        System.out.println("\n\n\nExercicis 2");
+
+        KeyPair claves = Claus.randomGenerate(1024);
+
+        PublicKey pubKey = claves.getPublic();
+        PrivateKey privateKey = claves.getPrivate();
+
+        byte[][] clauEmbEnc = Claus.encryptWrappedData(dataBy,pubKey);
+
+
+        byte[]  clauEmbDec = Claus.decryptWrappedData(clauEmbEnc,privateKey);
+
+        System.out.println(new String(clauEmbDec));
+    }
+}
+
